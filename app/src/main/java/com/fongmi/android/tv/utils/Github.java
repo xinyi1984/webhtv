f195bbee223c6bb4378a7a1cf5daba8f40acb0bc
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2026-08-11 12:42:04 +0800

    Update Github.java

--- app/src/main/java/com/fongmi/android/tv/utils/Github.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Github.java
@@ -2,12 +2,12 @@ package com.fongmi.android.tv.utils;
 
 public class Github {
 
-    private static final String GITHUB_LATEST = "https://github.com/fish2018/webhtv/releases/latest/download";
-    private static final String GITHUB_RELEASE = "https://github.com/fish2018/webhtv/releases/download";
-    private static final String GITHUB_API = "https://api.github.com/repos/fish2018/webhtv/releases/tags";
-    private static final String GITHUB_RELEASES_API = "https://api.github.com/repos/fish2018/webhtv/releases";
-    private static final String GITHUB_RELEASE_ASSETS_API = "https://api.github.com/repos/fish2018/webhtv/releases/assets";
-    private static final String CNB = "https://cnb.cool/fish2035/webhtv-release/-/git/raw/main";
+    private static final String GITHUB_LATEST = "https://github.com/xinyi1984/webhtv/releases/latest/download";
+    private static final String GITHUB_RELEASE = "https://github.com/xinyi1984/webhtv/releases/download";
+    private static final String GITHUB_API = "https://api.github.com/repos/xinyi1984/webhtv/releases/tags";
+    private static final String GITHUB_RELEASES_API = "https://api.github.com/repos/xinyi1984/webhtv/releases";
+    private static final String GITHUB_RELEASE_ASSETS_API = "https://api.github.com/repos/xinyi1984/webhtv/releases/assets";
+    private static final String CNB = "https://cnb.cool/ysysxy/webhtv-release/-/git/raw/main";
 
     public static String getCnbAsset(String name) {
         return CNB + "/apk/" + name;
